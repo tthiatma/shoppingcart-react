7e20c0dc425415d1247d3caa61ac073ac38553d6
Tonny Thiatmaja <tonny@tonnysolutions.com>
2023-02-04 22:12:15 -0700

    added api and ef

---
+++ API/Entities/Product.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public required string Name { get; set; }
+
+        public required string Description { get; set; }
+
+        public long Price { get; set; }
+
+        public required string PictureUrl { get; set; }
+
+        public required string Type { get; set; }
+
+        public required string Brand { get; set; }
+
+        public int QuantityInStock { get; set; }
+
+
+    }
+}
